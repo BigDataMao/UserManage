f0a7620a12d3501ed3d7b635a16920ac3f24e7ea
flinkmao <mxw712@gmail.com>
2023-09-11 18:09:05 +0800

    微调

--- users/views.py
+++ users/views.py
@@ -53,10 +53,14 @@ def group_edit(request, group_id):
     group_form = GroupForm(request.POST, instance=group)
     # 校验数据
     if group_form.is_valid():
+        # 亦可自定义一些数据
+        # group_form.instance.group_name = "自定义"
         # 保存数据
         group_form.save()
         # 跳转到用户组列表页面
         return redirect("/group/list/")
+    # 校验失败，group_form.errors会带上错误信息
+    return render(request, "group_edit.html", {"group_form": group_form})
 
 
 def group_delete(request, group_id):
@@ -114,10 +118,18 @@ def user_edit(request, user_id):
     user_form = UserForm(request.POST, instance=user)
     # 校验数据
     if user_form.is_valid():
+        # 亦可自定义校验规则
+        if user_form.cleaned_data["username"] == "admin":
+            user_form.add_error("username", "用户名不能为admin,你怕是要卡bug吗")
+            return render(request, "user_edit.html", {"user_form": user_form})
+        # 亦可制造埋点数据
+        # user_form.instance.xxx = "123456"
         # 保存数据
         user_form.save()
         # 跳转到用户列表页面
         return redirect("/user/list/")
+    # 校验失败，user_form.errors会带上错误信息
+    return render(request, "user_edit.html", {"user_form": user_form})
 
 
 def user_delete(request, user_id):
