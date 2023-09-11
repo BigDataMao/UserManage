cd5302b08caceaf6b40ae3c00ca100d40ed4f471
flinkmao <mxw712@gmail.com>
2023-09-11 11:24:33 +0800

    增删改查完成

--- UserManage/urls.py
+++ UserManage/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
 from users import views
@@ -22,7 +21,11 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path("test/", views.test),
     path("group/list/", views.group_list),
+    path("group/add/", views.group_add),
+    path("group/edit/<int:group_id>", views.group_edit),
+    path("group/delete/<int:group_id>/", views.group_delete),
     path("user/list/", views.user_list),
     path("user/add/", views.user_add),
+    path("user/edit/<int:user_id>/", views.user_edit),
     path("user/delete/<int:user_id>/", views.user_delete),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from users import models
+from django import forms
 
 
 # Create your views here.
@@ -12,12 +13,64 @@ def group_list(request):
     return render(request, "group_list.html", {"groups": groups})
 
 
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = models.Group
+        # fields = "__all__"  # 所有字段
+        fields = ["group_name"]
+
+    # 重写__init__方法
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 为每个字段添加样式
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["placeholder"] = f"请输入{field.label}"
+
+
+def group_add(request):
+    if request.method == "GET":
+        group_form = GroupForm()
+        return render(request, "group_add.html", {"group_form": group_form})
+    # 获取用户提交的数据
+    group_form = GroupForm(request.POST)
+    # 校验数据
+    if group_form.is_valid():
+        # 保存数据
+        group_form.save()
+        # 跳转到用户组列表页面
+        return redirect("/group/list/")
+
+
+def group_edit(request, group_id):
+    # 获取要编辑的用户组对象
+    group = models.Group.objects.get(id=group_id)
+    if request.method == "GET":
+        # 将用户组对象传递给前端页面
+        group_form = GroupForm(instance=group)
+        return render(request, "group_edit.html", {"group_form": group_form})
+    # 获取用户提交的数据
+    group_form = GroupForm(request.POST, instance=group)
+    # 校验数据
+    if group_form.is_valid():
+        # 保存数据
+        group_form.save()
+        # 跳转到用户组列表页面
+        return redirect("/group/list/")
+
+
+def group_delete(request, group_id):
+    # 获取要删除的用户组对象
+    group = models.Group.objects.get(id=group_id)
+    # 删除用户组
+    group.delete()
+    # 跳转到用户组列表页面
+    return redirect("/group/list/")
+
+
 def user_list(request):
     users = models.User.objects.all()
     return render(request, "user_list.html", {"users": users})
-
-
-from django import forms
 
 
 class UserForm(forms.ModelForm):
@@ -42,6 +95,23 @@ def user_add(request):
         return render(request, "user_add.html", {"user_form": user_form})
     # 获取用户提交的数据
     user_form = UserForm(request.POST)
+    # 校验数据
+    if user_form.is_valid():
+        # 保存数据
+        user_form.save()
+        # 跳转到用户列表页面
+        return redirect("/user/list/")
+
+
+def user_edit(request, user_id):
+    # 获取要编辑的用户对象
+    user = models.User.objects.get(id=user_id)
+    if request.method == "GET":
+        # 将用户对象传递给前端页面
+        user_form = UserForm(instance=user)
+        return render(request, "user_edit.html", {"user_form": user_form})
+    # 获取用户提交的数据
+    user_form = UserForm(request.POST, instance=user)
     # 校验数据
     if user_form.is_valid():
         # 保存数据
