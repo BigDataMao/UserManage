ec1f2da5843d3373387fd1b2944d7f44dcce166e
BigDataMao <mxw712@gmail.com>
2023-09-17 22:35:34 +0800

    主要功能完工

--- users/utils/code.py
+++ users/utils/code.py
@@ -2,7 +2,7 @@ import random
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 
-def check_code(width=120, height=30, char_length=5, font_file="users/static/fonts/LXGWWenKaiMono-Regular.ttf", font_size=28):
+def check_code(width=120, height=35, char_length=5, font_file="users/static/fonts/LXGWWenKaiMono-Regular.ttf", font_size=28):
     code = []
     img = Image.new(mode='RGB', size=(width, height), color=(255, 255, 255))
     draw = ImageDraw.Draw(img, mode='RGB')
